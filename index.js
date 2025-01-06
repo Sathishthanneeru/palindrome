4f1f95c80251107f599bbd4075e22cfb692ce1a7
Sathishthanneeru <sathishthanneeru36@gmail.com>
2025-01-06 19:49:00 +0530

    new

---
+++ index.js
@@ -0,0 +1,15 @@
+var isPalindrome=(str)=> {
+  let str1 = "";
+  for (i = str.length - 1; i >= 0; i--) {
+    str1 += str[i];
+  }
+//   console.log(str1);
+  if(str1==str){
+    console.log(`${str} is a palindrome`);
+  }
+  else{
+    console.log(`${str} is not a palindrome`)
+  }
+}
+isPalindrome("level");
+isPalindrome("malayalam")
