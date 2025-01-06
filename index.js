c9ac8e4964c3db421767f9a7dd45de4d129906a8
Sathishthanneeru <sathishthanneeru36@gmail.com>
2025-01-06 23:03:50 +0530

    new

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-var isPalindrome=(str)=> {
-  let str1 = "";
-  for (i = str.length - 1; i >= 0; i--) {
-    str1 += str[i];
-  }
-//   console.log(str1);
-  if(str1==str){
-    console.log(`${str} is a palindrome`);
-  }
-  else{
-    console.log(`${str} is not a palindrome`)
-  }
-}
-isPalindrome("level");
-isPalindrome("malayalam")
+// var isPalindrome=(str)=> {
+//   let str1 = "";
+//   for (i = str.length - 1; i >= 0; i--) {
+//     str1 += str[i];
+//   }
+// //   console.log(str1);
+//   if(str1==str){
+//     console.log(`${str} is a palindrome`);
+//   }
+//   else{
+//     console.log(`${str} is not a palindrome`)
+//   }
+// }
+// isPalindrome("level");
+// isPalindrome("malayalam")
+
+
+// const arr=[1,2,3]
+// arr.length=0;
+// console.log(arr[2])
 
 
 
+// function abc (){
+//   console.log(x);
+// }
+// abc()
+// var x=400;
+// var x=200
+// abc();
 
+
+// function sample(value){
+//   if(value){
+//     if(value > 10){
+//       return;
+//     }
+//   if(value % 2 ==0){
+//     return "even number";
+//   }
+//   return "odd number";
+//   }
+//   return "no value privided"
+// }
+// console.log(sample(12));
+// console.log(sample(8));
+// console.log(sample(2));
+// console.log(sample(null));
+// console.log(sample());
+// console.log(sample(undefined));
